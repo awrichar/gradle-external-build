1fae58a71c91d2d054fcf7af3e8fb0eecc271dd9
Andrew Richardson <awrichardson6@gmail.com>
2016-04-16 11:07:36 -0400

    Simplify BuildConfigContext

--- src/main/groovy/co/arichardson/gradle/make/context/BuildConfigContext.groovy
+++ src/main/groovy/co/arichardson/gradle/make/context/BuildConfigContext.groovy
@@ -18,20 +18,15 @@ class BuildConfigContext extends NativeBinaryContext {
         buildTask = task
     }
 
-    @Override
-    boolean equals(Object other) {
-        other in BuildConfigContext && buildTask == other.buildTask
-    }
-
-    Object methodMissing(String name, args) {
+    def methodMissing(String name, args) {
         buildTask."$name"(*args)
     }
 
-    Object propertyMissing(String name) {
-        return buildTask."$name"
+    def propertyMissing(String name) {
+        buildTask."$name"
     }
 
-    void propertyMissing(String name, value) {
+    def propertyMissing(String name, value) {
         buildTask."$name" = value
     }
 }
