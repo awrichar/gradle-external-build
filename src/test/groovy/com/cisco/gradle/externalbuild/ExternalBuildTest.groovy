d5dccc5b935fb70b4b03e833183470c973268ec6
Andrew Richardson <awrichardson6@gmail.com>
2017-06-26 15:23:25 -0400

    Add tests for library dependencies
    Test dependencies in both directions (Gradle component depends on
    external component and vice versa). These tests continue to feel hacky,
    but it's better than nothing. Need to continue finding better ways to
    verify that things are working as expected.

--- src/test/groovy/com/cisco/gradle/externalbuild/ExternalBuildTest.groovy
+++ src/test/groovy/com/cisco/gradle/externalbuild/ExternalBuildTest.groovy
@@ -6,6 +6,7 @@ import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 
+import static org.gradle.testkit.runner.TaskOutcome.FAILED
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class ExternalBuildTest extends Specification {
@@ -37,9 +38,13 @@ class ExternalBuildTest extends Specification {
         return succeed ? runner.build() : runner.buildAndFail()
     }
 
-    String outputText(String component, String buildType) {
+    String outputText(String component, String buildType=null) {
         File outputFolder = new File(testProjectDir.root, "build/tmp/externalBuild${component.capitalize()}")
-        return new File(outputFolder, "${buildType}-output.txt").text.trim()
+        if (buildType == null) {
+            return new File(outputFolder, 'output.txt').text.trim()
+        } else {
+            return new File(outputFolder, "${buildType}-output.txt").text.trim()
+        }
     }
 
     List<String> folderContents(File folder, String subfolder='') {
@@ -253,5 +258,97 @@ class ExternalBuildTest extends Specification {
         outputText('barExecutable', 'makeAll') == 'make-arg-1 make-arg-2 -j 1 all'
         folderContents(testProjectDir.root, 'build/exe/foo') == ['foo']
         folderContents(testProjectDir.root, 'build/exe/bar') == ['bar']
+    }
+
+    def "external executable depends on Gradle library"() {
+        given:
+        buildFile << """
+            $pluginInit
+
+            model {
+                components {
+                    foo(ExternalNativeExecutableSpec) {
+                        buildConfig(GnuMake) {
+                            lib library: 'bar'
+
+                            executable 'echo'
+                            args(*requiredLibraries)
+                        }
+
+                        buildOutput {
+                            outputFile = file('${outputFile.path}')
+                        }
+                    }
+
+                    bar(NativeLibrarySpec) {
+                        sources {
+                            cpp {
+                                source {
+                                    srcDir '.'
+                                    include 'lib.cpp'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        """
+
+        File srcFile = testProjectDir.newFile('lib.cpp')
+        srcFile.text = "int bar() { return 0; }"
+
+        when:
+        def result = runBuild()
+
+        then:
+        result.task(":build").outcome == SUCCESS
+        outputText('fooExecutable').contains('libbar.so')
+        folderContents(testProjectDir.root, 'build/exe/foo') == ['foo']
+    }
+
+    def "Gradle executable depends on external library"() {
+        given:
+        buildFile << """
+            $pluginInit
+
+            model {
+                components {
+                    foo(ExternalNativeLibrarySpec) {
+                        buildConfig(GnuMake) {
+                            executable 'echo'
+                        }
+
+                        buildOutput {
+                            outputFile = file('${outputFile.path}')
+                        }
+                    }
+
+                    bar(NativeExecutableSpec) {
+                        sources {
+                            cpp {
+                                source {
+                                    srcDir '.'
+                                    include 'main.cpp'
+                                }
+
+                                lib library: 'foo'
+                            }
+                        }
+                    }
+                }
+            }
+        """
+
+        File srcFile = testProjectDir.newFile('main.cpp')
+        srcFile.text = "int main() { return 0; }"
+
+        when:
+        def result = runBuild(false)
+
+        then:
+        result.task(":fooSharedLibrary").outcome == SUCCESS
+        result.task(":linkBarExecutable").outcome == FAILED
+        result.output.contains('libfoo.so')
+        folderContents(testProjectDir.root, 'build/libs/foo/shared') == ['libfoo.so']
     }
 }
